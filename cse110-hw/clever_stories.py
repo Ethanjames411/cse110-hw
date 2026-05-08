e54edcfa5919161174045dd17b52a682183b826b
Ethanjames411 <ewalker@ethanjstudio.com>
2026-05-08 21:36:01 +0000

    week project first version

---
+++ cse110-hw/clever_stories.py
@@ -0,0 +1,18 @@
+"""
+Author: Ethan James Walker
+
+Purpose: Display a story base on user inputs
+"""
+
+print("Please enter the following information: ")
+
+adjective = input("adjective: ")
+animal = input("animal: ")
+verb1 = input("verb: ")
+exclamation = input("exclamation: ").capitalize()
+verb2 = input("verb: ")
+verb3 = input("verb: ")
+
+print("\nYour story is:\n")
+
+print(f"The other day, I was really in trouble. It all started when I saw a very {adjective} {animal} {verb1} down the hallway. \"{exclamation}!\" I yelled. But all I could think to do was to {verb2} over and over. Miraculously, that caused it to stop, but not before it tried to {verb3} right in front of my family.")
